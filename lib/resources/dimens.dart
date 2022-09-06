f5939b8e810a7486a87c3f0b08f5d0ba8334e153
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2022-09-07 00:56:33 +0200

    Create dimens

---
+++ lib/resources/dimens.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Dimens {
+  static const double paddingXS = 2;
+  static const double paddingS = 4;
+  static const double paddingBase = 8;
+  static const double paddingMedium = 10;
+  static const double paddingLarge = 16;
+  static const double paddingXLarge = 32;
+  static const double paddingXXLarge = 64;
+  static const double paddingXXXLarge = 110;
+
+  static const double textSizeTitle = 24;
+  static const double textSizeSubtitle = 22;
+  static const double textSizeBody = 19;
+  static const double textSizeBodySmall = 17;
+  static const double textSizeBodyTiny = 15;
+
+  static const FontWeight fontWeightLight = FontWeight.w300;
+  static const FontWeight fontWeightMedium = FontWeight.w500;
+  static const FontWeight fontWeightRegular = FontWeight.normal;
+  static const FontWeight fontWeightBold = FontWeight.bold;
+
+  static const double radiusSmall = 4;
+  static const double radiusMedium = 8;
+  static const double radiusLarge = 16;
+}
