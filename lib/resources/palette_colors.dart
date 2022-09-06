7ffffd8d6879ec7b2999ecdb81856b1efd69afa6
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2022-09-07 00:56:15 +0200

    Create palette colors

---
+++ lib/resources/palette_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaletteColors {
+  static const Color background = Colors.white70;
+  static const Color primary = Color(0xFFc0cbff);
+  static const Color secondary = Color(0xFFffc0cb);
+  static const Color text = Colors.black;
+  static const Color textSubtitle = Colors.black54;
+}
