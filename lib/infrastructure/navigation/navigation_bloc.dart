008e38b81e7a493cb2e5edcb8f6226998c7bf3fc
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2022-09-07 01:15:41 +0200

    Make code more readable

--- lib/infrastructure/navigation/navigation_bloc.dart
+++ lib/infrastructure/navigation/navigation_bloc.dart
@@ -8,17 +8,11 @@ class NavigatorBloc extends Bloc<NavigationEvent, dynamic> {
   final NavigationService _navigationService = locator<NavigationService>();
 
   NavigatorBloc(initialState) : super(initialState) {
-    on(
-      (event, emit) {
-        if (event is HomeNavigationEvent) {
-          _navigationService.replace(Routes.home);
-        } else if (event is BackNavigationEvent) {
-          _navigationService.goBack();
-        } else {
-          event as PushScreenNavigationEvent;
-          _navigationService.navigateTo(event.model.route, arguments: event.model.arguments);
-        }
-      },
-    );
+    on<HomeNavigationEvent>((event, emit) => _navigationService.replace(Routes.home));
+    on<BackNavigationEvent>((event, emit) => _navigationService.goBack());
+    on<PushScreenNavigationEvent>((event, emit) => _navigationService.navigateTo(
+          event.model.route,
+          arguments: event.model.arguments,
+        ));
   }
 }
